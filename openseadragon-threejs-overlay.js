dc461f2e6d1551d38bb099796b8ee5a842428760
unknown <abc@example.com>
2018-11-10 15:49:54 -0600

    orthographic camera

--- openseadragon-threejs-overlay.js
+++ openseadragon-threejs-overlay.js
@@ -52,6 +52,8 @@
 
         this._renderer = this.renderer();
         this._camera = undefined;
+        this._cameraOrtho = undefined;
+
         this._scene = undefined;
         this._stats = undefined;
         // this.imagingHelper = this._viewer.activateImagingHelper();
@@ -100,6 +102,28 @@
             console.log("three zoom", this._camera.zoom);
             return this._camera;
         },
+
+
+        cameraOrtho: function () {
+            
+			var SCREEN_WIDTH = window.innerWidth;
+			var SCREEN_HEIGHT = window.innerHeight;
+			var aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+			var frustumSize = 600;
+
+            if (this._cameraOrtho) return this._cameraOrtho;
+            this._cameraOrtho = new THREE.OrthographicCamera( 0.5 * frustumSize * aspect / - 2, 0.5 * frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 0, 1000 );
+            zoomIndex=1;
+            this._cameraOrtho.left=(zoomIndex *0.5 * frustumSize * aspect / - 2) + 50;
+            this._cameraOrtho.right=(zoomIndex * 0.5 * frustumSize * aspect / 2) + 50;
+            this._cameraOrtho.top=zoomIndex*frustumSize / 2;
+            this._cameraOrtho.bottom=zoomIndex*frustumSize / - 2;
+            this._cameraOrtho.updateProjectionMatrix();
+            this._cameraOrtho.updateProjectionMatrix();
+            console.log("three zoom", this._cameraOrtho.zoom);
+            return this._cameraOrtho;
+        },
+
         scene: function () {
             return this._scene ? this._scene : new THREE.Scene();
         },
@@ -107,23 +131,37 @@
             // console.log("Scene coordinates: ", x, y)
             x = Math.round(x);
             y = Math.round(y);
-            this._vec.set(
-                (x / window.innerWidth) * 2 - 1,
-                - (y / window.innerHeight) * 2 + 1,
-                0.5);
+            
             // this._vec.set(
+            //         (x / window.innerWidth) * 2 - 1,
+            //         - (y / window.innerHeight) * 2 + 1,
+            //         0);
+            // // this._vec.set(
             //     (x / this.width) * 2 - 1,
             //     - (y / this.height) * 2 + 1,
             //     0.5);
+            browX=(x / window.innerWidth) * 2 - 1;
+            browY=- (y / window.innerHeight) * 2 + 1;
+            browZ=0.5;
+            let browserCoord=new THREE.Vector3(browX,browY,browZ);
+            console.log('browserCoord', browserCoord );
+            browserCoord.unproject(this._cameraOrtho);
+            console.log('browserCoord unproject', browserCoord );
 
-            // console.log(distance, camera, vec);
-            this._vec.unproject(this._camera);
+            // posX=0.5*this._cameraOrtho.right+0.5*this._cameraOrtho.left;
+            // posY=0.5*this._cameraOrtho.top+0.5*this._cameraOrtho.bottom;
+            // let posOrtho=new THREE.Vector3(posX,posY,0);
+            // console.log('posOrtho',posOrtho);
+            // this._vec.unproject(this._camera);
+            // this._vec.sub(this._camera.position).normalize();
+            // var distance = - this._camera.position.z / this._vec.z;
 
-            this._vec.sub(this._camera.position).normalize();
-
-            var distance = - this._camera.position.z / this._vec.z;
-            this._pos.copy(this._camera.position).add(this._vec.multiplyScalar(distance));
-            // console.log("World coordinates: ", this._pos);
+            // this._pos.copy(this._camera.position).add(this._vec.multiplyScalar(distance));
+            this._pos.copy(browserCoord);
+            this._pos.z=0;
+            console.log('camera limits ',this._cameraOrtho.left,this._cameraOrtho.right,this._cameraOrtho.bottom,this._cameraOrtho.top,this._cameraOrtho.far,this._cameraOrtho.near);
+            console.log("World coordinates: ", this._pos);
+    
             return this._pos;
         },
         stats: function () {
